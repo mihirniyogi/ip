b4b86a48912f26b641386085e497dbb7cad1a1a5
mihirniyogi <E1122052@u.nus.edu>
2025-01-21 14:36:57 +0800

    Add Bob's logo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,7 +1,18 @@
 public class Bob {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
-        System.out.println(line + "\nHello! I'm Bob\nWhat can I do for you?");
+        String logo = """
+                         ____     _____    ____
+                        |  _  \\\s |     |  |  _  \\
+                        | |_| /  |  _  |  | |_| /      
+                        |____/   | | | |  |____/      / \\/ \\
+                        |  _  \\\s | |_| |  |  _  \\     \\    /
+                        | |_| /  |     |  | |_| /      \\  /
+                        |____/   |_____|  |____/        \\/
+                        """;
+
+        System.out.println(logo);
+        System.out.println(line + "\nHello! I'm Bob ♥ \nWhat can I do for you?");
         System.out.println(line + "\nThank you and goodbye!\n" + line);
     }
 }
