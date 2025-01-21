e6a417fd7beb755ede3680f97c2766d77b3dc8ba
mihirniyogi <E1122052@u.nus.edu>
2025-01-21 13:58:38 +0800

    Duke.java: Change Duke to Bob

---
+++ src/main/java/Bob.java
@@ -0,0 +1,7 @@
+public class Bob {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line + "\nHello! I'm Bob\nWhat can I do for you?");
+        System.out.println(line + "\nThank you and goodbye!\n" + line);
+    }
+}
