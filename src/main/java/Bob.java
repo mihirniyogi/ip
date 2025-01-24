a31bcac4d086699b82286d0f27ca3cb6898a8a89
mihirniyogi <E1122052@u.nus.edu>
2025-01-24 12:01:44 +0800

    Enable task unmarking

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -7,21 +7,34 @@ public class Bob {
 
         Helper.printLogo();
         Helper.print("Hello! I'm Bob ♥", "What can I do for you?");
-        
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             String userInput = scanner.nextLine();
+            
+            // exit
             if (userInput.equals("bye")) {
                 break;
+            
+            // list out
             } else if (userInput.equals("list")) {
                 tasks.print();
                 continue;
+            
+            // mark task
             } else if (userInput.startsWith("mark")) {
-                // mark n
                 int number = Integer.parseInt(userInput.split(" ")[1]);
                 tasks.markTask(number);
                 String taskString = tasks.getTask(number).toString();
                 Helper.print("Bob is on it! Marked the following as done:", taskString);
+                continue;
+            
+            // unmark task
+            } else if (userInput.startsWith("unmark")) {
+                int number = Integer.parseInt(userInput.split(" ")[1]);
+                tasks.unmarkTask(number);
+                String taskString = tasks.getTask(number).toString();
+                Helper.print("Bob is on it! Marked the following as undone:", taskString);
                 continue;
             }
             
