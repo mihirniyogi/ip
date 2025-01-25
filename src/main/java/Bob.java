e19e5406881063129168aa939bd19fb2913b89c4
mihirniyogi <E1122052@u.nus.edu>
2025-01-25 12:41:41 +0800

    Clean up code

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -22,30 +22,25 @@ public class Bob {
                 // list out
                 } else if (userInput.trim().equals("list")) {
                     printTasks();
-                    continue;
                 
                 // mark task
                 } else if (userInput.startsWith("mark")) {
                     int number = Integer.parseInt(userInput.split(" ")[1]);
                     markTask(number);
                     Helper.print("Bob is on it! Marked the following as done [X]:", taskList.get(number - 1).toString());
-                    continue;
                 
                 // unmark task
                 } else if (userInput.startsWith("unmark")) {
                     int number = Integer.parseInt(userInput.split(" ")[1]);
                     unmarkTask(number);
                     Helper.print("Bob is on it! Marked the following as undone [ ]", taskList.get(number - 1).toString());
-                    continue;
                 
                 // todo
                 } else if (userInput.startsWith("todo")) {
                     String[] parts = userInput.split(" ", 2);
-    
                     if (parts.length == 1 || parts[1].isBlank()) {
                         throw new WrongCommandException("Uh oh! Bob says...the description of a todo cannot be empty.");
                     }
-    
                     String description = parts[1];
                     Task task = new Todo(description);
                     addTask(task);
@@ -54,20 +49,15 @@ public class Bob {
                 // deadline
                 } else if (userInput.startsWith("deadline")) {
                     String[] command = userInput.split(" ", 2);
-
                     if (command.length == 1 || command[1].isBlank()) {
                         throw new WrongCommandException("Uh oh! Bob says...the description of a deadline cannot be empty.");
                     }
-
                     String[] parts = command[1].split(" /by ");
-                    
                     if (parts.length == 1 || parts[1].isBlank()) {
                         throw new WrongCommandException("Uh oh! Bob says...a deadline task needs a '/by'. e.g. deadline return book /by Sunday");
                     }
-
                     String description = parts[0];
                     String by = parts[1];
-
                     Task task = new Deadline(description, by);
                     addTask(task);
                     Helper.print("Bob is on it! I've added this task:", task.toString(), "Now you have " + taskList.size() + " task(s).");
@@ -75,23 +65,18 @@ public class Bob {
                 // event
                 } else if (userInput.startsWith("event")) {
                     String[] command = userInput.split(" ", 2);
-                    
                     if (command.length == 1 || command[1].isBlank()) {
                         throw new WrongCommandException("Uh oh! Bob says...the description of an event cannot be empty.");
                     }
                     String[] parts = command[1].split(" /from ");
-
                     if (parts.length == 1 || parts[1].isBlank()) {
                         throw new WrongCommandException("Uh oh! Bob says...an event task needs a '/from' and '/to'. e.g. event project meeting /from Monday 2pm /to Monday 4pm");
                     }
-
                     String description = parts[0];
                     String[] dates = parts[1].split(" /to ");
-
                     if (dates.length == 1 || dates[1].isBlank()) {
                         throw new WrongCommandException("Uh oh! Bob says...an event task needs a '/to'. e.g. event project meeting /from Monday 2pm /to Monday 4pm");
-                    } 
-                    
+                    }
                     String from = dates[0];
                     String to = dates[1];
                     Task task = new Event(description, from, to);
