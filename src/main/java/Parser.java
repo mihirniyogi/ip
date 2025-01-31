d07bad98265e30c4320a9202b03b05805748f4ba
mihirniyogi <E1122052@u.nus.edu>
2025-01-31 12:19:02 +0800

    Add basic parse method

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Parser {
-    
+    public static Command parse(String userInput) throws WrongCommandException, IOException {
+        throw new WrongCommandException("Unrecognised command!");
+    }
 }
