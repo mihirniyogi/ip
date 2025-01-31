917dd21bdde204c58c08a87361a56d8798f36ce1
mihirniyogi <E1122052@u.nus.edu>
2025-01-31 12:20:17 +0800

    Add ExitCommand

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,6 +3,9 @@ import java.time.LocalDateTime;
 
 public class Parser {
     public static Command parse(String userInput) throws WrongCommandException, IOException {
+        if (userInput.equals("bye")) {
+            return new ExitCommand();
+        }
         throw new WrongCommandException("Unrecognised command!");
     }
 }
