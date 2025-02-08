1710529ba44d1934068e323b41ce730c461044a8
mihirniyogi <E1122052@u.nus.edu>
2025-02-08 22:14:44 +0800

    Make minimal echo GUI

--- src/main/java/bob/gui/Main.java
+++ src/main/java/bob/gui/Main.java
@@ -1,19 +1,28 @@
 package bob.gui;
 
+import bob.Bob;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    private Bob bob = new Bob(); 
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label label = new Label();
-        label.setText("Hello, Bob!");
-
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Bob");
-        primaryStage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = loader.load();  
+            Scene scene = new Scene(ap);
+            primaryStage.setScene(scene);
+            loader.<MainWindow>getController().setBob(this.bob);
+            primaryStage.setTitle("Bob");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }   
 }
---
+++ src/main/java/bob/gui/MainWindow.java
@@ -0,0 +1,37 @@
+package bob.gui;
+
+import bob.Bob;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    
+    @FXML
+    private VBox dialogContainer;
+
+    @FXML
+    private TextField userInput;
+
+    @FXML
+    private Button sendButton;
+
+    private Bob bob;
+
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = input; // TODO: To be changed
+        dialogContainer.getChildren().add(DialogBox.getUserDialog(input, null));
+        userInput.clear();
+    }
+
+    public void setBob(Bob b) {
+        this.bob = b;
+    }
+}
