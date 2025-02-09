296c0f5ba7a08e7b58fd2c83480d85dbe79bbeeb
mihirniyogi <E1122052@u.nus.edu>
2025-02-09 11:48:22 +0800

    Remove unnecessary code - initialize is auto-called anyway

--- src/main/java/bob/gui/Main.java
+++ src/main/java/bob/gui/Main.java
@@ -1,6 +1,5 @@
 package bob.gui;
 
-import bob.Bob;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,20 +8,13 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private Bob bob = new Bob(); 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = loader.load();  
             Scene scene = new Scene(ap);
-            primaryStage.setScene(scene);
-            
-            MainWindow controller = loader.getController();
-            controller.setBob(this.bob);
-            controller.initialize();
-            
+            primaryStage.setScene(scene);            
             primaryStage.setTitle("Bob");
             primaryStage.show();
         } catch (Exception e) {
