e69b6b29a3f30384c6c6725c3b4cd376f81b94ad
mihirniyogi <E1122052@u.nus.edu>
2025-02-11 17:22:21 +0800

    Add comments to increase readability of Parser

--- src/main/java/bob/parser/Parser.java
+++ src/main/java/bob/parser/Parser.java
@@ -51,37 +51,51 @@ public class Parser {
 
         if (userInput.startsWith("find")) {
             String[] parts = userInput.split(" ");
+
+            // if user gives empty search term
             if (parts.length == 1) {
                 throw new WrongCommandException("Uh oh! Bob says...the search term cannot be empty.");
             }
+
+            // if user gives more than 1 word as search term
             if (parts.length > 2) {
                 throw new WrongCommandException("Uh oh! Bob says...the search term must only be 1 word.");
             }
+
             String searchTerm = parts[1];
             return new FindCommand(searchTerm);
         }
 
         if (userInput.startsWith("todo")) {
             String[] parts = userInput.split(" ", 2);
+
+            // if user gives empty description
             if (parts.length == 1 || parts[1].isBlank()) {
                 throw new WrongCommandException("Uh oh! Bob says...the description of a todo cannot be empty.");
             }
+
             String description = parts[1];
             return new TodoCommand(description);
         }
 
         if (userInput.startsWith("deadline")) {
             String[] command = userInput.split(" ", 2);
+
+            // if user gives empty description
             if (command.length == 1 || command[1].isBlank()) {
                 throw new WrongCommandException("Uh oh! Bob says...the description of a deadline cannot be empty.");
             }
+
             String[] parts = command[1].split(" /by ");
+
+            // if user gives empty '/by'
             if (parts.length == 1 || parts[1].isBlank()) {
                 throw new WrongCommandException("""
                         Uh oh! Bob says...a deadline task needs a '/by'.
                         e.g. deadline return book /by 18/02/2025 1800
                         """);
             }
+
             String description = parts[0];
             String byInput = parts[1];
             LocalDateTime by = Helper.inputToDateTime(byInput);
@@ -90,24 +104,33 @@ public class Parser {
 
         if (userInput.startsWith("event")) {
             String[] command = userInput.split(" ", 2);
+
+            // if user gives empty description
             if (command.length == 1 || command[1].isBlank()) {
                 throw new WrongCommandException("Uh oh! Bob says...the description of an event cannot be empty.");
             }
+
             String[] parts = command[1].split(" /from ");
+
+            // if user gives empty 'from'
             if (parts.length == 1 || parts[1].isBlank()) {
                 throw new WrongCommandException("""
                         Uh oh! Bob says...an event task needs a '/from' and '/to'.
                         e.g. event project meeting /from 17/04/2025 1400 /to 17/04/2025 1600
                         """);
             }
+
             String description = parts[0];
             String[] dates = parts[1].split(" /to ");
+
+            // if user gives empty 'to'
             if (dates.length == 1 || dates[1].isBlank()) {
                 throw new WrongCommandException("""
                         Uh oh! Bob says...an event task needs a '/to'.
                         e.g. event project meeting /from 17/04/2025 1400 /to 17/04/2025 1600
                         """);
             }
+
             String fromInput = dates[0];
             String toInput = dates[1];
 
