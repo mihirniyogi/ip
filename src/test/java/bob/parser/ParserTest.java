41e7a69980f4bb67ce920a354d495aad29da7e98
mihirniyogi <E1122052@u.nus.edu>
2025-02-16 20:55:57 +0800

    Add more valid and invalid input testing

--- src/test/java/bob/parser/ParserTest.java
+++ src/test/java/bob/parser/ParserTest.java
@@ -1,5 +1,6 @@
 package bob.parser;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -7,12 +8,20 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 import bob.command.Command;
+import bob.command.DeadlineCommand;
+import bob.command.DeleteCommand;
+import bob.command.EventCommand;
 import bob.command.ExitCommand;
 import bob.command.ListCommand;
 import bob.command.MarkCommand;
+import bob.command.TodoCommand;
 import bob.command.UnmarkCommand;
 import bob.command.WrongCommandException;
 
+/**
+ * Tests whether, based on a given user input, whether the correct command will be returned.
+ * Does NOT execute the command.
+ */
 public class ParserTest {
     @Test
     public void testParseExit_validInput_returnsExitCommand() throws WrongCommandException, IOException {
@@ -43,15 +52,57 @@ public class ParserTest {
     }
 
     @Test
-    public void testParseInvalidInput_throwsWrongCommandException() {
+    public void testParseTodo_validInput_returnsTodoCommand() throws WrongCommandException, IOException {
+        String userInput = "todo homework";
+        Command c = Parser.parse(userInput);
+        assertTrue(c instanceof TodoCommand);
+    }
+
+    @Test
+    public void testParseDeadline_validInput_returnsDeadlineCommand() throws WrongCommandException, IOException {
+        String userInput = "deadline assignment /by 01/03/2025 2359";
+        Command c = Parser.parse(userInput);
+        assertTrue(c instanceof DeadlineCommand);
+    }
+
+    @Test
+    public void testParseEvent_validInput_returnsEventCommand() throws WrongCommandException, IOException {
+        String userInput = "event meeting /from 05/03/2025 1400 /to 05/03/2025 1630";
+        Command c = Parser.parse(userInput);
+        assertTrue(c instanceof EventCommand);
+    }
+
+    @Test
+    public void testParseDelete_validInput_returnsDeleteCommand() throws WrongCommandException, IOException {
+        String userInput = "delete 1";
+        Command c = Parser.parse(userInput);
+        assertTrue(c instanceof DeleteCommand);
+    }
+
+    @Test
+    public void testParseMark_invalidNonNumericalInput_throwsNumberFormatException() {
+        String userInput = "unmark abc";
+
+        assertThrows(NumberFormatException.class, () -> {
+            Parser.parse(userInput);
+        });
+    }
+
+    @Test
+    public void testParseUnmark_invalidNonNumericalInput_throwsNumberFormatException() {
+        String userInput = "unmark abc";
+
+        assertThrows(NumberFormatException.class, () -> {
+            Parser.parse(userInput);
+        });
+    }
+
+    @Test
+    public void testParse_invalidInput_throwsWrongCommandException() {
         String userInput = "blah blah blah";
-        try {
-            @SuppressWarnings("unused")
-            Command c = Parser.parse(userInput);
-        } catch (WrongCommandException e) {
-            assertTrue(true);
-        } catch (IOException e) {
-            assertTrue(false);
-        }
+
+        assertThrows(WrongCommandException.class, () -> {
+            Parser.parse(userInput);
+        });
     }
 }
