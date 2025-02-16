409153b128be84ca6d9b42cce189f5aa46ba6aa5
mihirniyogi <E1122052@u.nus.edu>
2025-02-16 12:43:25 +0800

    Refactor to shorten parse method

---
+++ src/main/java/bob/parser/ParseFunction.java
@@ -0,0 +1,13 @@
+package bob.parser;
+
+import bob.command.Command;
+import bob.command.WrongCommandException;
+
+/**
+ * This functional interface represents a function that takes in a string and returns a Command.
+ * It is implemented by the numerous parse functions in Parser.
+ */
+@FunctionalInterface
+public interface ParseFunction {
+    Command apply(String userInput) throws WrongCommandException;
+}
